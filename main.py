883d6649e6dcff0e09b41c10257049cb026f49c4
g-pale <saburkin.s@outlook.com>
2025-07-18 12:46:05 +0300

    add new class

---
+++ main.py
@@ -0,0 +1,28 @@
+class Warrior():
+    def __init__(self, name, power, endurance, hair_colour):
+        self.name = name
+        self.power = power
+        self.endurance = endurance
+        self.hair_colour = hair_colour
+
+    def sleep(self):
+        print(f'{self.name} лег спать')
+        self.endurance += 2
+
+    def eat(self):
+        print(f'{self.name} сел кушать')
+        self.power += 1
+
+    def hit(self):
+        print(f'{self.name} бьет кого-то')
+        self.endurance -= 6
+
+    def walk(self):
+        print(f'{self.name} гуляет')
+
+    def info(self):
+        print(f'имя воина - {self.name}')
+        print(f'цвет волос - {self.hair_colour}')
+        print(f'сила воина - {self.power}')
+        print(f'выносливость воина - {self.endurance}')
+
